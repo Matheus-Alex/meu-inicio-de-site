1a5b8c5794431ca3ea0e9dbf01e37d1a42d8c4cc
Matheus-Alex <matheusalex2303@gmail.com>
2025-11-19 16:21:03 -0300

    Add files via upload

---
+++ OutrasCoisas/script.js
@@ -0,0 +1,17 @@
+function mudarCor(){
+    const quadrado = document.getElementById('quadrado')
+    quadrado.style.backgroundColor = 'blue'
+}
+
+function Desfazer(){
+    const quadrado = document.getElementById('quadrado')
+    return quadrado.style.backgroundColor = 'yellow'
+}
+
+function mudarcor(){
+    document.body.style.backgroundColor = "#CDE6D0"
+}
+
+function voltarcor(){
+    document.body.style.backgroundColor = "white"
+}
